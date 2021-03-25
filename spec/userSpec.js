b12c0f7cad2026251bbe18bcb571c73b761f23bd
Ábner Neves <abnermneves@gmail.com>
2021-03-24 23:24:54 -0300

    testes acrescentados no spec

--- spec/userSpec.js
+++ spec/userSpec.js
@@ -1,29 +1,26 @@
-const mongoose = require('mongoose')
-const server = require('../index')
-
-describe("Testes de unidade", function() {
-    const user = require('../src/scripts/cadastrarUsuario.js');
-
-    describe("Testes - cadastro de usuário", function() {
-        it("Usuário não preenchido deve ser nulo", function() {
+describe("Suite com alguns testes de unidade", function() {
+    
+    describe("Cadastro de usuário", function() {
+        it("Testar se usuário não preenchido retorna nulo", function() {
             expect().nothing();
+            const user = require('../src/scripts/cadastrarUsuario.js');
             expect(user.estaNulo).toBeUndefined();
         });
 
-        it("Cadastrar usuário vazio não deve ser bem sucedido", function() {
+        it("Testar se cadastrar usuário vazio não é bem sucedido", function() {
             const user = require('../src/scripts/cadastrarUsuario.js');
             expect(user.cadastrarUsuario).toBeUndefined();
         });
 
-    });
-    
-    describe("Testes - cadastro de plano", function() {
-        it("", function() {
-
+        it("Testar se senhas de um usuário vazio não são validadas", function() {
+            const user = require('../src/scripts/cadastrarUsuario.js');
+            expect(user.validaSenhas).toBeUndefined();
         });
 
-        it("", function() {
-
+        it("Dados JSON de um ", function() {
+            const user = require('../src/scripts/cadastrarUsuario.js');
+            expect(user.getDadosJSON).toBeUndefined();
         });
+
     });
 });
